e237c1e215e75ea46f76f5e9142ef86b183e370b
Paul Kinlan <paul.kinlan@gmail.com>
2023-06-04 18:45:58 +0000

    Adding more auth

---
+++ islands/AuthForm.tsx
@@ -0,0 +1,36 @@
+type Props = {
+  mode: "In" | "Up";
+};
+
+export default function AuthForm({ mode }: Props) {
+  const signIn = {
+    title: "Sign In",
+    href: "/sign-in",
+    text: "Have an account?",
+  };
+
+  const signUp = {
+    title: "Create account",
+    href: "/sign-up",
+    text: "No account?",
+  };
+
+  const buttProps = mode == "In" ? signIn : signUp;
+
+  return (
+    <div class="items-stretch min-w-0">
+      <div class="flex justify-center">
+        <h2 class="my-4">{buttProps.title}</h2>
+      </div>
+
+      <form method="post" class="flex flex-col space-y-4 min-w-0">
+
+        <input type="email" name="email" placeholder="Email" class="form-control" />
+        <input type="password" name="password" placeholder="Password" class="form-control" />
+       
+        <input type="submit" value={buttProps.title} class="btn btn-primary" />
+       
+      </form>
+    </div>
+  );
+}
--- routes/sign-in.tsx
+++ routes/sign-in.tsx
@@ -1,49 +1,29 @@
 import { Head } from "$fresh/runtime.ts";
-import { Pool } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
+import { getCookies } from "std/http/cookie.ts";
+import { AuthForm } from "../islands/AuthForm.tsx";
 
 export type Data = {
-    isAllowed: boolean;
+  isAllowed: boolean;
 };
 
 export const handler: Handlers = {
-    GET(req, ctx) {
-        const cookies = getCookies(req.headers);
-        return ctx.render({ isAllowed: cookies.auth == "superzitrone" });
-    }
+  GET(req, ctx) {
+    const cookies = getCookies(req.headers);
+    return ctx.render({ isAllowed: cookies.auth == "superzitrone" });
+  }
 }
 
-
 export default function Home() {
-    // Get the connection string from the environment variable "DATABASE_URL"
-    const databaseUrl = Deno.env.get("DATABASE_URL")!;
+  // Get the connection string from the environment variable "DATABASE_URL"
 
-    // Create a database pool with three connections that are lazily established
-    const pool = new Pool(databaseUrl, 3, true);
-
-    // Connect to the database
-    const connection = await pool.connect();
-
-    try {
-        // Create the table
-        await connection.queryObject`
-    CREATE TABLE IF NOT EXISTS todos (
-      id SERIAL PRIMARY KEY,
-      title TEXT NOT NULL
-    )
-  `;
-    } finally {
-        // Release the connection back into the pool
-        connection.release();
-    }
-
-    return (
-        <>
-            <Head>
-                <title>Sign In</title>
-            </Head>
-            <div class="p-4 mx-auto max-w-screen-md">
-                <button>Sign In</button>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>Sign In</title>
+      </Head>
+      <div class="p-4 mx-auto max-w-screen-md">
+       <AuthForm mode="In">
+      </div>
+    </>
+  );
 }
